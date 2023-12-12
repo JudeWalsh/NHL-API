25f6b4b8610325fb4776ac537681c347b789b851
JudeWalsh <walshjud@msu.edu>
2023-12-11 21:21:20 -0500

    Player Bio and Summary stats

--- NHL-Wrapper.py
+++ NHL-Wrapper.py
@@ -1,8 +1,109 @@
 import requests
+import json
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
 
 class NHLAPIWrapper:
     def __init__(self, base_url='https://api-web.nhle.com/', stats_url='https://api.nhle.com/stats/rest'):
         self.base_url = base_url
         self.stats_url = stats_url
 
-    # Add your API methods here
+    def GetPlayerStats(self):
+        for i in range(8):
+            start = i*100
+            url = self.stats_url + f"/en/skater/summary?limit=100&start={start}&sort=points&cayenneExp=seasonId=20232024"
+            # Make a GET request
+            response = requests.get(url)
+
+            # Check if the request was successful (status code 200)
+            if response.status_code == 200:
+                # Parse the JSON content of the response
+                data = response.json()
+                print(json.dumps(data, indent=4))
+            else:
+                # Print an error message if the request was not successful
+                print("Error:", response.status_code, response.text)
+
+    def playerBio(self, playerid):
+        url = self.base_url + f"v1/player/{playerid}/landing"
+
+        response = requests.get(url)
+
+        data = response.json()
+
+        final = {}
+
+        final['height'] = data['heightInInches']
+        final['weight'] = data['weightInPounds']
+        final['position'] = data['position']
+        final['shootscatches'] = data['shootsCatches']
+
+        birthdate = data['birthDate']
+        date_format = "%Y-%m-%d"
+
+        # Convert the string to a datetime object
+        birthdate = datetime.strptime(birthdate, date_format)
+
+        current_datetime = datetime.now()
+
+        difference = relativedelta(current_datetime, birthdate)
+
+        # Extract the number of years from the difference
+        age = difference.years
+
+        final['age'] = age
+
+        draft_year = int(data['draftDetails']['year'])
+
+        final['draft year'] = draft_year
+
+        return final
+
+    def playerSummary(self, playerid):
+        url = self.base_url + f"v1/player/{playerid}/landing"
+
+        response = requests.get(url)
+
+        data = response.json()
+
+        '''
+        games played
+        goals
+        assists
+        points
+        plus minus
+        pim
+        p\GP
+        EVG
+        EVP
+        PPG
+        PPP
+        SHG
+        SHP
+        OTG
+        GWG
+        S
+        S%
+        TOI/GP
+        FOW%
+        '''
+
+        curr_season_stats = data['seasonTotals'][-1]
+
+        keys_to_remove = ['gameTypeId', 'leagueAbbrev',
+                          'teamName', 'sequence']
+
+        for key in keys_to_remove:
+            if key in curr_season_stats:
+                curr_season_stats.pop(key)
+
+        return curr_season_stats
+
+
+obj = NHLAPIWrapper()
+
+info = obj.playerSummary(8476453)
+
+print(json.dumps(info, indent=4))
+
--- README.md
+++ README.md
@@ -1,2 +1,85 @@
 # NHL-API
 Wrapper for new NHL API
+
+# Predictive Claims Scoring
+
+## Get Started
+
+Clone this repository into your local machine.
+
+Locate in your terminal or open in an IDE to access
+
+# Run the application
+
+## Create a Virtual Environment
+
+In your terminal type the following command
+```
+python -m venv venv
+```
+
+Our interpreter uses python 3.9.16
+
+## Activate Virtual Environment
+For windows run the following command
+```
+venv\Scripts\activate
+```
+
+For Mac:
+```
+source venv/bin/activate
+```
+
+## Install necessary libraries
+```
+pip install -r requirements.txt
+```
+
+## Deactivate virtual environment
+```
+deactivate
+```
+
+## Freeze libraries
+If you decide to install new libraries, 
+run the following command
+
+```
+pip freeze > requirements.txt
+```
+
+# Concept
+The purpose of this wrapper is to be able to easily access 
+any of the information on the front page of the NHL website
+
+The types of reports are the following:
+Summary
+Bio Info
+Faceoff percentages
+Faceoff Wins and losses
+Goals For and Against
+Miscellaneous
+Penalties
+Penalty Kill
+Penalty Shots
+Power Play
+Puck possession
+SAT Counts
+SAT Percentages
+Scoring per 60
+Scoring per game
+Shootout
+Shots by type
+Time on Ice
+
+## Summary
+
+## Bio Info
+
+To access player Bio Information
+
+use the function ```playerBio(playerID)```
+
+to return the players height in inches, weight in pounds and current age
+in the form of a dict
